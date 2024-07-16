cb852de9c43bdcb613a1b36611ff24f97b52b8d7
jdahmenlemur <142763346+jdahmen-splunk@users.noreply.github.com>
2024-07-16 11:18:06 -0700

    auto instrumentation solution added

--- ios/Demo Boutique/Demo Boutique/AppDelegate.swift
+++ ios/Demo Boutique/Demo Boutique/AppDelegate.swift
@@ -1,11 +1,24 @@
 import UIKit
 import SDWebImage
+import SplunkOtel
+import SplunkOtelCrashReporting
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //set the realm and token
+
+        SplunkRumBuilder(realm: "<realm>", rumAuth: "<rum-token>")
+        // Call methods to configure environment name, app name, and if you want to see debug logs
+        .deploymentEnvironment(environment: "<environment>")
+        .setApplicationName("<your_app_name>").debug(enabled: true).build()
+
+        //enable crash reporting
+
+        SplunkRumCrashReporting.start()
+        
 
         // Clear SDWebImage caches so the images get loaded on every app open
         SDImageCache.shared.clearMemory()
